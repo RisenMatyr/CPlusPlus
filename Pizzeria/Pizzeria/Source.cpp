07d6b5508da59b9d48ae6d8c546ba4b0bff46cf7
RisenMatyr <RisenMatyr@gmail.com>
2017-04-10 09:48:43 -0500

    Update

---
+++ Pizzeria/Pizzeria/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//double mustard = 4.20;
+	//double mayonnaise = 5.00;
+	//double ketchup = 6.66;
+	double total;
+	//double large = 13;
+	//double medium = 7;
+	//double small = 5;
+	int pizzaYes = 1;
+	int pizzaNo = 2;
+	int small = 1;
+	int medium = 2;
+	int large = 3;
+	int mustard = 1;
+	int mayonnaise = 2;
+	int ketchup = 3;
+
+
+	cout << "Welcome too Steamers Pizza! We will roll you out of here in a wheelchair.";
+	cout << "\nNow you have a few things you can choose from.";
+	cout << "Would you like to order a pizza? 1 for yes 2 for no";
+	cin >> pizzaYes;
+	if (pizzaYes == 1)
+	{
+		cout << "What size pizza would you like? 1 for small, 2 for medium, and 3 for large.";
+		if (small == 1)
+		{
+			
+		}
+
+	}
+
+}
