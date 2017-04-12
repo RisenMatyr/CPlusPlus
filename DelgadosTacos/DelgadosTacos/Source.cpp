51d366729c2e7a892b3c78dc49f3a20c49a1e948
RisenMatyr <RisenMatyr@gmail.com>
2017-04-12 10:33:33 -0500

    Update

--- DelgadosTacos/DelgadosTacos/Source.cpp
+++ DelgadosTacos/DelgadosTacos/Source.cpp
@@ -4,80 +4,132 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <iomanip>
 
 int main()
 {
-	double tacoPrice = 1.50;
-	double burritoPrice = 2.00;
-	double drinkPrice = 1.50;
-	double tamalePrice = 2.00;
-	double tostadoPrice = 3.00;
-	double enchiladaPrice = 3.00;
-	double churroPrice = 0.50;
-	double quesadillaPrice = 2.50;
-	double chimichangaPrice = 1.50;
-	double chalupaPrice = 2.50;
-
+	double tacoPrice = 1.50, burritoPrice = 2.00, drinkPrice = 1.50, tamalePrice = 2.00, tostadoPrice = 3.00,
+		   enchiladaPrice = 3.00, churroPrice = 0.50, quesadillaPrice = 2.50, chimichangaPrice = 1.50,
+	       chalupaPrice = 2.50;
 	const double taxRate = 1.075;
+	double tacoOrdered, tacoTotal, burritoOrdered, burritoTotal, drinkOrdered, drinkTotal, tamaleOrdered, tamaleTotal
+		, tostadoOrdered, tostadoTotal, enchiladaOrdered, enchiladaTotal, churroOrdered, churroTotal, quesadillaOrdered, quesadillaTotal
+		, chimichangaOrdered, chimichangaTotal, chalupaOrdered, chalupaTotal, total;
+	int order;
 
-	double tacoOrdered, tacoTotal;
-	double burritoOrdered, burritoTotal;
-	double drinkOrdered, drinkTotal;
-	double tamaleOrdered, tamaleTotal;
-	double tostadoOrdered, tostadoTotal;	
-	double enchiladaOrdered, enchiladaTotal;
-	double churroOrdered, churroTotal;
-	double quesadillaOrdered, quesadillaTotal;
-	double chimichangaOrdered, chimichangaTotal;
-	double chalupaOrdered, chalupaTotal;
-	double total;
-	double totalWithTax;
+	std::cout << "Welcome to Delgados Tacos, where we will force our food down your throat and\nsteal your money.\n" << std::endl;
 
-	std::cout << "Welcome to Delgados Tacos, where we will force our food down your throat and steal your money." << std::endl;
+	std::cout << "MAKE your choice between taco(1), burrito(2), drink(3), tamale(4), tostado(5),\nenchiliada(6), churro(7), quesadilla(8), chimichanga(9), and chalupa(10).\n\n";
+	system("PAUSE");
+	std::cin >> order;
 
-	std::cout << "You're going to be force fed this many tacos down your throat: " << std::endl;
-	std::cin >> tacoOrdered;
+	if (order == 1)
+	{
+		std::cout << "You're going to be force fed this many tacos down your throat: " << std::endl;
+		std::cin >> tacoOrdered;
 
-	std::cout << "Then we're going to shove this many burritos down your throat: " << std::endl;
-	std::cin >> burritoOrdered;
+		tacoTotal = tacoOrdered * tacoPrice;
+		total = tacoTotal * taxRate;
 
-	std::cout << "Than you're going to choke on this many sodas: " << std::endl;
-	std::cin >> drinkOrdered;
+		std::cout << "You're missing "<< std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
 
-	std::cout << "We're going to jam this many tamales into your mouth: " << std::endl;
-	std::cin >> tamaleOrdered;
+	else if (order == 2)
+	{
+		std::cout << "Then we're going to shove this many burritos down your throat: " << std::endl;
+		std::cin >> burritoOrdered;
 
-	std::cout << "Your're going to suffer through this many tostados: " << std::endl;
-	std::cin >> tostadoOrdered;
+		burritoTotal = burritoOrdered * burritoPrice;
+		total = burritoTotal * taxRate;
 
-	std::cout << "We'll attack you with this many enchiladas: " << std::endl;
-	std::cin >> enchiladaOrdered;
+		std::cout << "You're missing " << std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
 
-	std::cout << "You can fit this many churros in your mouth at once: " << std::endl;
-	std::cin >> churroOrdered;
+	else if (order == 3)
+	{
+		std::cout << "Than you're going to choke on this many sodas: " << std::endl;
+		std::cin >> drinkOrdered;
 
-	std::cout << "You will choke on this many quesadillas: " << std::endl;
-	std::cin >> quesadillaOrdered;
+		drinkTotal = drinkOrdered * drinkPrice;
+		total = drinkTotal * taxRate;
 
-	std::cout << "I'm running out of things to say... \n"  "How many chimichangas did you want?: " << std::endl;
-	std::cin >> chimichangaOrdered;
+		std::cout << "You're missing " << std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
 
-	std::cout << "Finally how many chalupas did you want?: " << std::endl;
-	std::cin >> chalupaOrdered;
+	else if (order == 4)
+	{
+		std::cout << "We're going to jam this many tamales into your mouth: " << std::endl;
+		std::cin >> tamaleOrdered;
 
-	tacoTotal = tacoOrdered * tacoPrice;
-	burritoTotal = burritoOrdered * burritoPrice;
-	drinkTotal = drinkOrdered * drinkPrice;
-	tamaleTotal = tamaleOrdered * tamalePrice;
-	tostadoTotal = tostadoOrdered * tostadoPrice;
-	enchiladaTotal = enchiladaOrdered * enchiladaPrice;
-	churroTotal = churroOrdered * churroPrice;
-	quesadillaTotal = quesadillaOrdered * quesadillaPrice;
-	chimichangaTotal = chimichangaOrdered * chimichangaPrice;
-	chalupaTotal = chalupaOrdered * chalupaPrice;
+		tamaleTotal = tamaleOrdered * tamalePrice;
+		total = tamaleTotal * taxRate;
 
-	total = tacoTotal + burritoTotal + drinkTotal + tamaleTotal + tostadoTotal + enchiladaTotal 
-		  + churroTotal + quesadillaTotal + chimichangaTotal + chalupaTotal;
+		std::cout << "You're missing " << std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
 
-	std::cout << "You're missing " << total << "$ from you're wallet. Come again!" << std::endl;
+	else if (order == 5)
+	{
+		std::cout << "You're going to suffer through this many tostados: " << std::endl;
+		std::cin >> tostadoOrdered;
+
+		tostadoTotal = tostadoOrdered * tostadoPrice;
+		total = tostadoTotal * taxRate;
+
+		std::cout << "You're missing " << std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
+
+	else if (order == 6)
+	{
+		std::cout << "We'll attack you with this many enchiladas: " << std::endl;
+		std::cin >> enchiladaOrdered;
+
+		enchiladaTotal = enchiladaOrdered * enchiladaPrice;
+		total = enchiladaTotal * taxRate;
+
+		std::cout << "You're missing " << std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
+
+	else if (order == 7)
+	{
+		std::cout << "You can fit this many churros in your mouth at once: " << std::endl;
+		std::cin >> churroOrdered;
+
+		churroTotal = churroOrdered * churroPrice;
+		total = churroTotal * taxRate;
+
+		std::cout << "You're missing " << std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
+
+	else if (order == 8)
+	{
+		std::cout << "You will choke on this many quesadillas: " << std::endl;
+		std::cin >> quesadillaOrdered;
+
+		quesadillaTotal = quesadillaOrdered * quesadillaPrice;
+		total = quesadillaTotal * taxRate;
+
+		std::cout << "You're missing " << std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
+
+	else if (order == 9)
+	{
+		std::cout << "I'm running out of things to say... \n"  "How many chimichangas did you want?: " << std::endl;
+		std::cin >> chimichangaOrdered;
+
+		chimichangaTotal = chimichangaOrdered * chimichangaPrice;
+		total = chimichangaTotal * taxRate;
+
+		std::cout << "You're missing " << std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
+
+	else if (order == 10)
+	{
+		std::cout << "Finally how many chalupas did you want?: " << std::endl;
+		std::cin >> chalupaOrdered;
+
+		chalupaTotal = chalupaOrdered * chalupaPrice;
+		total = chalupaTotal * taxRate;
+
+		std::cout << "You're missing " << std::setprecision(3) << total << "$ from you're wallet. Come again!" << std::endl;
+	}
 }
--- Pizzeria/Pizzeria/Source.cpp
+++ Pizzeria/Pizzeria/Source.cpp
@@ -3,13 +3,10 @@ using namespace std;
 
 int main()
 {
-	//double mustard = 4.20;
-	//double mayonnaise = 5.00;
-	//double ketchup = 6.66;
 	double total;
-	//double large = 13;
-	//double medium = 7;
-	//double small = 5;
+	
+	int pizzaCondiments;
+	int pizzaSize;
 	int pizzaYes = 1;
 	int pizzaNo = 2;
 	int small = 1;
@@ -19,8 +16,7 @@ int main()
 	int mayonnaise = 2;
 	int ketchup = 3;
 
-
-	cout << "Welcome too Steamers Pizza! We will roll you out of here in a wheelchair.";
+	cout << "Welcome to Steamers Pizza! We will roll you out of here in a wheelchair.";
 	cout << "\nNow you have a few things you can choose from.";
 	cout << "Would you like to order a pizza? 1 for yes 2 for no";
 	cin >> pizzaYes;
@@ -28,10 +24,34 @@ int main()
 	{
 		cout << "What size pizza would you like? 1 for small, 2 for medium, and 3 for large.";
 		if (small == 1)
-		{
-			
-		}
+		{pizzaSize = 5.00;}
+		if (medium == 2)
+		{pizzaSize = 7.00;}
+		if (large == 3)
+		{pizzaSize = 13.00;}
 
+		system("pause");
+
+		cout << "What is your toppings? 1 for Mustard. 2 for Ketchup, 3 for Mayonnaise";
+		if (mustard == 1)
+		{pizzaCondiments = 4.20;}
+		if (ketchup == 2)
+		{pizzaCondiments = 6.66;}
+		if (mayonnaise == 3)
+		{pizzaCondiments = 5.00;}
+
+		system("pause");
+
+		total = pizzaSize + pizzaCondiments;
+
+		cout << "Your total cost adds up to " << total;
+	}
+
+	system("pause");
+
+	if (pizzaNo == 2)
+	{
+		cout << "Then get out of the damn pizzeria!!!";
 	}
 
 }
